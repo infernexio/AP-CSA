64e60d3f6eaf7b8a364c83d7ff6a6c7e4959fc30
sohailshaik <sohailshaik@localhost>
2019-12-17 22:20:04 -0500

    The Start of the OldmacDonald Project just barly started just trying to learn Git with eclipse

---
+++ Old-MacDonald/SSAbstractAnimal.java
@@ -0,0 +1,36 @@
+/*
+* @author - Sohail Shaik
+* @Date - Dec 17,2019
+* @Assignment - SSAbstractAnimal
+*/
+
+public class SSAbstractAnimal implements SSAnimal{
+  private String sound;
+  private String type;
+
+  /*
+  *set the initial state of the var
+  *@param - animaltype- is the type of animal
+  *@param - animalsound - is the the sound the animal makes
+  */
+  public SSAbstractAnimal(String animaltype, String animalsound){
+    sound = animalsound;
+    type = animaltype;
+  }
+
+  /*
+  *reutrn the cound of the animal
+  *@returns - the sound of the animal
+  */
+  public String getSound(){
+    return sound;
+  }
+
+  /*
+  *returns the type of the animal
+  *@returns - the type of animal this is
+  */
+  public String getType(){
+    return type;
+  }
+}
---
+++ Old-MacDonald/SSAnimal.java
@@ -0,0 +1,12 @@
+/*
+* @author - Sohail Shaik
+* @Date - Dec 17,2019
+* @Assignment - SSAnimal
+*/
+
+public interface SSAnimal{
+
+ public abstract String getSound();
+ public abstract String getType();
+ 
+}
