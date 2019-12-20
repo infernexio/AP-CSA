e9246c67fc5c8d372aaaea9c187756fde5b95abd
sshaik22 <sshaik22@THS-138-19.websterschools.net>
2019-12-20 14:58:44 -0500

    Fixed toString third time

--- Old-MacDonald/SSNamedCow.java
+++ Old-MacDonald/SSNamedCow.java
@@ -28,6 +28,6 @@ public class SSNamedCow extends SSCow{
 	  *@returns - the type of animal this is
 	  */
 	public String getType() {
-		return "Confused " + super.getType();
+		return "Named " + super.getType();
 	}
 }
